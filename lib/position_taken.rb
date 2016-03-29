be0c5f19faa475ccaa4f6fbf8b1bdb9dbf8de908
Wen Tran <wentran@Wen-Trans-MacBook-Pro.local>
2016-03-28 22:50:31 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+ if (board[position] === " ") || (board[position] === nil) || (board[position] === "")
+  false
+else
+  true
+  end
+
+end
